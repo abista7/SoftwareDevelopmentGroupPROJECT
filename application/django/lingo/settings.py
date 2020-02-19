d285d4b3dfa886fdaeaab2e334acb1a66a1175da
Ubuntu <ubuntu@ip-172-31-31-167.us-west-2.compute.internal>
2020-02-19 23:55:28 +0000

    setting.py

--- application/django/lingo/settings.py
+++ application/django/lingo/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'lingo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lingo',
+        'USER': 'lingouser',
+        'PASSWORD': 'HDo!n5!eW54z6*E#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

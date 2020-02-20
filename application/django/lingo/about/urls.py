3a91f370c9e6994d6758e26226d464881d4133f8
Ryan Hu <213edu@gmail.com>
2020-02-19 20:24:07 -0800

    about us page
    prob a very wrong way to do this but i dont know how to server static html in django so
    
    -init about app
    -routed traffic via url
    -view for each page

---
+++ application/django/lingo/about/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('arjun', views.arjun, name='arjun'),
+    path('mariam', views.mariam, name='mariam'),
+    path('ryan', views.ryan, name='ryan'),
+    path('sawara', views.sawara, name='sawara'),
+    path('dylan', views.dylan, name='dylan'),
+]

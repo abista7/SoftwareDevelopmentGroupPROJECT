d47462d77c3727a8c023c77299f89490c32e23c4
Ryan Hu <213edu@gmail.com>
2020-04-16 18:16:33 -0700

    Frontend changes: - any static js/css can be place under /application/django/mainapp/static/ - any template file is now in /application/django/templates

--- application/django/lingomingo/settings.py
+++ application/django/lingomingo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -25,11 +24,7 @@ SECRET_KEY = '95(&@nca!mkxk)6on1_4go9)4nmj2y%q8q@kivx0=np&o)e(hp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = ['lingomingo.app', '34.213.215.231', '0.0.0.0']
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -59,8 +54,7 @@ ROOT_URLCONF = 'lingomingo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lingomingo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -85,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -105,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -118,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- application/django/mainapp/models.py
+++ application/django/mainapp/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import uuid
-
-
+import pycountry
 # Create your models here.
 
 class Profile(models.Model):

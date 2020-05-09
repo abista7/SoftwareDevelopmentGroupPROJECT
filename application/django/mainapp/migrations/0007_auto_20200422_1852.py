3147ee3a84aefb484f78415861c81b4d2210dc74
dylan <dluong1@mail.sfsu.edu>
2020-05-09 16:56:21 -0700

    fixed migrations, and post works with frontend

--- application/django/mainapp/migrations/0007_auto_20200422_1852.py
+++ application/django/mainapp/migrations/0007_auto_20200422_1852.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0006_post_like'),
+        ('mainapp', '0006_profile_about_me'),
     ]
 
     operations = [

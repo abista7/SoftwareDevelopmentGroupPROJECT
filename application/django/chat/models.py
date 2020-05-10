d81e6c533a8cbaf5d2810b77d78669ca1b535777
Ryan Hu <213edu@gmail.com>
2020-05-09 17:26:40 -0700

    save

--- application/django/chat/models.py
+++ application/django/chat/models.py
@@ -9,14 +9,24 @@ class Thread(models.Model):
     user1 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='thread_user_1', null=True)
     user2 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='thread_user_2', null=True)
 
+    def __str__(self):
+        return str(self.user1) + ' and ' + str(self.user2) + '\'s thread'
+
     def get_messages(self):
         return Message.objects.filter(thread=self)
+
+    def get_last_message(self):
+        return Message.objects.filter(thread=self).last()
 
 
 def get_or_create_thread(user1, user2):
     if Thread.objects.filter(Q(user1=user1, user2=user2) | Q(user1=user2, user2=user1)).count() < 1:
         Thread.objects.create(user1=user1, user2=user2)
     return Thread.objects.get(Q(user1=user1, user2=user2) | Q(user1=user2, user2=user1))
+
+
+def get_thread_by_user(user):
+    return Thread.objects.filter(Q(user1=user) | Q(user2=user))
 
 
 class Message(models.Model):

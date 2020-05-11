5e1416ba5271de32aa20bcfdf9d3d88937abdb2c
Ryan Hu <213edu@gmail.com>
2020-05-10 21:05:31 -0700

    clean up

--- application/django/chat/views.py
+++ application/django/chat/views.py
@@ -5,10 +5,6 @@ from mainapp.models import get_profile_model
 from .models import get_or_create_thread, Message
 
 
-def chat(request):
-    return render(request, 'chat/index.html', {})
-
-
 def room(request, other_profile_uuid=''):
     other_profile = get_profile_model().get(uuid=other_profile_uuid)
     thread = get_or_create_thread(user1=request.user, user2=other_profile.user)

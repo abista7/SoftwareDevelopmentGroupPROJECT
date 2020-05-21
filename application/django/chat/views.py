3ef9d574a6e0194519ac026c77c0bf7c1d990d90
Ryan Hu <213edu@gmail.com>
2020-05-21 11:50:42 -0700

    plug chat with UI

--- application/django/chat/views.py
+++ application/django/chat/views.py
@@ -2,12 +2,17 @@
 from django.shortcuts import render
 
 from mainapp.models import get_profile_model
-from .models import get_or_create_thread, Message
+from .models import *
 
 
 def room(request, other_profile_uuid=''):
-    other_profile = get_profile_model().get(uuid=other_profile_uuid)
-    thread = get_or_create_thread(user1=request.user, user2=other_profile.user)
-    messages = Message.objects.filter(thread=thread)
-    context = {'room_name': thread.id, 'messages': messages}
+    context = {}
+    if other_profile_uuid:
+        other_profile = get_profile_model().get(uuid=other_profile_uuid)
+        thread = get_or_create_thread(user1=request.user, user2=other_profile.user)
+        messages = Message.objects.filter(thread=thread)
+        context.update({'room_name': thread.id, 'msgs': messages, 'thread': thread})
+
+    all_thread = get_thread_by_user(request.user)
+    context.update({'all_thread': all_thread})
     return render(request, 'chat/room.html', context)
